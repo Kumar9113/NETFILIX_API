0c7a05fe2d273f207bdc96b6127560a49642289c
Kumar9113 <86244075+Kumar9113@users.noreply.github.com>
2023-06-26 22:35:47 +0530

    Add files via upload

---
+++ verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function verify(req, res, next) {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token,"eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ",(err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+}
+
+module.exports = verify;
